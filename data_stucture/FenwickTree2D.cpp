614cbc774454da95f471ccb62bb831bb6a840e0b
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2019-02-26 09:36:40 +0600

    Create FenwickTree2D.cpp

---
+++ data_stucture/FenwickTree2D.cpp
@@ -0,0 +1,141 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define pii pair<int,int>
+#define ll long long
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vii vector<pii>
+#define vvii vector<vii>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define MOD 1000000007
+#define EPS 1e-8
+#define mem(x,y) memset(x,y,sizeof x)
+#define INF 100000000
+#define dist2D(x1,y1,x2,y2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+#define dist3D(x1,y1,z1,x2,y2,z2) ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))
+#define sortv(v) sort(v.begin(),v.end())
+#define FASTIO ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define PI acos(-1.0)
+#define FI freopen("in.txt","r",stdin)
+#define FO freopen("out.txt","w",stdout)
+#define debug(x) cout<<"WTH: "<<x<<endl
+#define fof(i,x,y) for(int i=x;i<(int)y;i++)
+#define fob(i,x,y) for(int i=x;i>=(int)y;i--)
+
+template<class T>
+class compare{
+public:
+    bool operator()(pair<T,T> x,pair<T,T> y){
+        if(x.first==y.first){
+            return x.ss>y.ss;
+        }
+        return x.ff>y.ff;
+    }
+};
+
+template<class T>
+ostream& operator<<(ostream &os,const pair<T,T> &a){
+    os<<a.ff<<" "<<a.ss;
+}
+template<class T>
+void print(vector<T> vec){
+    fof(i,0,vec.size()) cout<<vec[i]<<" "; cout<<endl;
+}
+
+template<class T> pair<T,T> operator+(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff+b.ff,a.ss+b.ss}; }
+template<class T> pair<T,T> operator-(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff-b.ff,a.ss-b.ss}; }
+template<class T> pair<T,T> operator*(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff*b.ff,a.ss*b.ss}; }
+template<class T> pair<T,T> operator%(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff%b.ff,a.ss%b.ss}; }
+template<class T,class U> pair<T,T> operator+(const pair<T,T> &a,const U &b){ return { a.ff+b,a.ss+b}; }
+template<class T,class U> pair<T,T> operator*(const pair<T,T> &a,const U &b){ return { a.ff*b,a.ss*b}; }
+
+int Set(int N,int pos) { return N=N|(1<<pos); }
+int reset(int N,int pos){ return N=N&~(1<<pos); }
+bool check(int N,int pos){  return (bool) (N&(1<<pos));}
+
+///=======================================template=======================================///
+
+#define LSOne(n) (n&(-n))
+
+class FenwickTree2D{
+    vvi ft;
+    int n,m;
+public:
+    FenwickTree2D(vvi ara){
+        n=ara.size();
+        m=ara[0].size();
+        ft.assign(n+1,vi());
+
+        fof(i,0,n){
+            ft[i+1].assign(m+1,0);
+        }
+
+        fof(i,0,n){
+            fof(j,0,m){
+                add(i+1,j+1,ara[i][j]);
+            }
+        }
+    }
+
+    void add(int x,int y,int val){
+        for(int i=x;i<ft.size();i+=LSOne(i)){
+            for(int j=y;j<ft[i].size();j+=LSOne(j)){
+                ft[i][j]+=val;
+            }
+        }
+    }
+
+    ll rsq(int x,int y){
+        ll sum=0;
+        for(int i=x;i>0;i-=LSOne(i)){
+            for(int j=y;j>0;j-=LSOne(j)){
+                sum+=ft[i][j];
+            }
+        }
+        return sum;
+    }
+
+    ll rsq(int dx,int dy,int ux,int uy){
+        return rsq(ux,uy)-rsq(dx-1,uy)-rsq(ux,dy-1)+rsq(dx-1,dy-1);
+    }
+};
+
+int main()
+{
+    //FI;FO;
+    FASTIO;
+
+    int n,m;
+    vvi vec;
+    cin>>n>>m;
+
+    vec.resize(n);
+    fof(i,0,n){
+        vec[i].resize(m);
+        fof(j,0,m){
+            int x; cin>>x;
+            vec[i][j]=x;
+        }
+    }
+
+    FenwickTree2D ft(vec);
+
+    while(true){
+
+        int ux,uy,dx,dy;
+        cin>>dx>>dy>>ux>>uy;
+        cout<<ft.rsq(dx,dy,ux,uy)<<endl;
+        ft.add(ux,uy,dx);
+
+    }
+
+
+    return 0;
+}
