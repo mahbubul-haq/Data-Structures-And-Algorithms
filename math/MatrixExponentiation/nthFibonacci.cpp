6e6df964cf151e993989347131970373706c5764
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2019-06-12 20:28:19 +0600

    Create nthFibonacci.cpp

---
+++ math/MatrixExponentiation/nthFibonacci.cpp
@@ -0,0 +1,142 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dist2D(x1,y1,x2,y2)         ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+#define dist3D(x1,y1,z1,x2,y2,z2)   ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))
+#define EPS                         1e-12
+#define FastIO                      ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define FI                          freopen("in.txt","r",stdin)
+#define FO                          freopen("out.txt","w",stdout)
+#define fap(x)                      cout<<"WTH: "<<x<<endl
+#define ff                          first
+#define fof(i,x,y)                  for(int i=x;i<(int)y;i++)
+#define fob(i,x,y)                  for(int i=x;i>=(int)y;i--)
+#define INF                         1000000000
+#define ld                          long double
+#define ll                          long long
+#define mem(x,y)                    memset(x,y,sizeof x)
+#define mp                          make_pair
+#define msi                         map<string,int>
+#define mii                         map<int,int>
+#define mis                         map<int,string>
+#define MOD                         2147483647
+#define PI                          acos(-1.0)
+#define PQ                          priority_queue
+#define pb                          push_back
+#define pib                         pair<int,bool>
+#define pii                         pair<int,int>
+#define pll                         pair<ll,ll>
+#define sfi(x)                      scanf("%d",&x)
+#define sfii(x,y)                   scanf("%d%d",&x,&y)
+#define sfiii(x,y,z)                scanf("%d%d%d",&x,&y,&z)
+#define siz(x)                      (int)x.size()
+#define sortv(v)                    sort(v.begin(),v.end())
+#define ss                          second
+#define ull                         unsigned long long
+#define umsi                        unordered_map<string,int>
+#define umii                        unordered_map<int,int>
+#define umis                        unordered_map<int,string>
+#define vb                          vector<bool>
+#define vi                          vector<int>
+#define vvi                         vector<vi>
+#define vii                         vector<pii>
+#define vvii                        vector<vii>
+#define vll                         vector<ll>
+#define vpll                        vector<pll>
+
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional>
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+        tree_order_statistics_node_update> ordered_set;
+
+//os.order_of_key(v): returns how many elements strictly less than v
+//os.find_by_order(k-1): returns kth smallest element's iterator
+*/
+
+template<class T> class compare{
+public:
+    bool operator()(pair<T,T> &x,pair<T,T> &y){
+        if(x.first==y.first){
+            return x.ss>y.ss;
+        }
+        return x.ff>y.ff;
+    }
+};
+
+template<class T> ostream& operator<<(ostream &os,const pair<T,T> &a) { os<<a.ff<<" "<<a.ss; }
+template<class T> ll power(T a,int b){
+    ll po=1; while(b--) po*=a; return po;
+}
+
+template<class T> pair<T,T> operator+(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff+b.ff,a.ss+b.ss}; }
+template<class T> pair<T,T> operator-(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff-b.ff,a.ss-b.ss}; }
+template<class T> pair<T,T> operator*(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff*b.ff,a.ss*b.ss}; }
+template<class T> pair<T,T> operator%(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff%b.ff,a.ss%b.ss}; }
+template<class T,class U> pair<T,T> operator+(const pair<T,T> &a,const U &b){ return { a.ff+b,a.ss+b}; }
+template<class T,class U> pair<T,T> operator*(const pair<T,T> &a,const U &b){ return { a.ff*b,a.ss*b}; }
+
+int Set(int N,int pos) { return N=N|(1<<pos); }
+int reset(int N,int pos){ return N=N&~(1<<pos);}
+bool check(int N,int pos){  return (bool) (N&(1<<pos));}
+
+///=======================================template=======================================///
+
+///Fibonacci number
+///F(0)=0,F(1)=1, F(n)=F(n-1)+F(n-2);
+
+int f[2]={0,1};
+
+void multiply(int mata[2][2],int matb[2][2]){
+    int mul[2][2];
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            mul[i][j]=0;
+            for(int k=0;k<2;k++){
+                mul[i][j]+=mata[i][k]*matb[k][j];
+            }
+        }
+    }
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++)
+            mata[i][j]=mul[i][j];
+    }
+}
+
+int power(int mat[2][2],int n){
+    int mul[2][2]={{1,1},{1,0}};
+    if(n==1){
+        return mat[0][0]*f[1]+mat[0][1]*f[0];
+    }
+    power(mat,n/2);
+    multiply(mat,mat);
+    if(n&1)
+        multiply(mat,mul);
+    return mat[0][0]*f[1]+mat[0][1]*f[0];
+}
+
+int findNthTerm(int n){
+    if(n<2) return f[n];
+    int mat[2][2]={{1,1},{1,0}};
+    return power(mat,n-1);
+}
+
+int main()
+{
+    //FI;FO;
+    FastIO;
+
+    cout<<findNthTerm(10)<<endl;
+    cout<<findNthTerm(2)<<endl;
+    cout<<findNthTerm(3)<<endl;
+    cout<<findNthTerm(4)<<endl;
+    cout<<findNthTerm(5)<<endl;
+    cout<<findNthTerm(6)<<endl;
+    cout<<findNthTerm(7)<<endl;
+    cout<<findNthTerm(8)<<endl;
+    cout<<findNthTerm(9)<<endl;
+
+    return 0;
+}
