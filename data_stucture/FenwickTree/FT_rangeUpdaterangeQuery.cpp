5c1060f45f1cf5f5eaff76c5052b394ca1f32082
Md Mahbubul Haque <44994405+Md-Mahbubul-Haque@users.noreply.github.com>
2019-11-23 08:53:49 +0600

    Update FT_rangeUpdaterangeQuery.cpp

--- data_stucture/FenwickTree/FT_rangeUpdaterangeQuery.cpp
+++ data_stucture/FenwickTree/FT_rangeUpdaterangeQuery.cpp
@@ -1,152 +1,175 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <cstring>
+#include <string>
+#include <set>
+#include <complex>
+#include <map>
+#include <unordered_map>
+#include <climits>
+#include <bitset>
+#include <queue>
+#include <chrono>
+#include <random>
+#include <typeinfo>
+#include <stack>
+#include <list>
 using namespace std;
 
-#define dist2D(x1,y1,x2,y2)         ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
-#define dist3D(x1,y1,z1,x2,y2,z2)   ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))
-#define EPS                         1e-12
-#define FastIO                      ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
-#define FI                          freopen("in.txt","r",stdin)
-#define FO                          freopen("out.txt","w",stdout)
-#define fap(x)                      cout<<"WTH: "<<x<<endl
+#define all(v)                      v.begin(), v.end()
+#define dist2D(x1, y1, x2, y2)            ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))
+#define dist3D(x1, y1, z1, x2, y2, z2)    ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2))
+#define EPS                         1e-16
+#define endl                        "\n"
+#define FastIO                      ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define FI                          freopen("in.txt", "r", stdin)
+#define FO                          freopen("out.txt", "w", stdout)
 #define ff                          first
-#define fof(i,x,y)                  for(int i=x;i<(int)y;i++)
-#define fob(i,x,y)                  for(int i=x;i>=(int)y;i--)
-#define INF                         100000
+#define INF              1000000000000000000
 #define ld                          long double
 #define ll                          long long
-#define mem(x,y)                    memset(x,y,sizeof x)
+#define mem(x,y)                    memset(x, y, sizeof x)
 #define mp                          make_pair
-#define msi                         map<string,int>
-#define mii                         map<int,int>
-#define mis                         map<int,string>
+#define msi                         map<string, int>
+#define mii                         map<int, int>
+#define mis                         map<int, string>
 #define MOD                         1000000007
 #define PI                          acos(-1.0)
 #define PQ                          priority_queue
 #define pb                          push_back
-#define pib                         pair<int,bool>
-#define pii                         pair<int,int>
-#define pll                         pair<ll,ll>
-#define sfi(x)                      scanf("%d",&x)
-#define sfii(x,y)                   scanf("%d%d",&x,&y)
-#define sfiii(x,y,z)                scanf("%d%d%d",&x,&y,&z)
+#define pib                         pair<int, bool>
+#define pii                         pair<int, int>
+#define pll                         pair<ll, ll>
+#define present(c, x)               ((c).find(x) != (c).end())
+#define sfi(x)                      scanf("%d", &x)
+#define sfii(x,y)                   scanf("%d %d", &x, &y)
+#define sfiii(x,y,z)                scanf("%d %d %d", &x, &y, &z)
 #define siz(x)                      (int)x.size()
-#define sortv(v)                    sort(v.begin(),v.end())
 #define ss                          second
-#define umsi                        unordered_map<string,int>
-#define umii                        unordered_map<int,int>
-#define umis                        unordered_map<int,string>
+#define sRand                       srand(chrono::steady_clock::now().time_since_epoch().count())
+#define timeTaken                   endd = clock(); cout << (double) (endd - beginn) / CLOCKS_PER_SEC * 1000 << endl
+#define timeInit                    clock_t beginn = clock(), endd
+#define ull                         unsigned long long
+#define umsi                        unordered_map<string, int>
+#define umii                        unordered_map<int, int>
+#define umis                        unordered_map<int, string>
 #define vb                          vector<bool>
 #define vi                          vector<int>
 #define vvi                         vector<vi>
 #define vii                         vector<pii>
 #define vvii                        vector<vii>
 #define vll                         vector<ll>
+#define vvll                        vector<vll>
 #define vpll                        vector<pll>
+#define bug(...)                    cerr << __PRETTY_FUNCTION__ << " - " << __LINE__ << " : (" << #__VA_ARGS__ << ") = ("; _Print(__VA_ARGS__);
+template<class T> void _Print(T &&x) { cerr << x << ")" << endl; }
+template<class T, class ...S> void _Print(T &&x, S &&...y) { cerr << x << ", "; _Print(y...); }
 
-template<class T> class compare{
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional>
+using namespace __gnu_pbds;
+typedef tree<int,null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update> ordered_set;
+
+//os.order_of_key(v): returns how many elements strictly less than v
+//os.find_by_order(k - 1): returns kth smallest element's iterator
+*/
+
+template<class T> class compare {
 public:
-    bool operator()(pair<T,T> &x,pair<T,T> &y){
-        if(x.first==y.first){
-            return x.ss>y.ss;
+    bool operator() (pair<T, T> &x, pair<T, T> &y) {
+        if (x.first == y.first) {
+            return x.ss > y.ss;
         }
-        return x.ff>y.ff;
+        return x.ff > y.ff;
     }
 };
 
-template<class T> ostream& operator<<(ostream &os,const pair<T,T> &a) { os<<a.ff<<" "<<a.ss; }
-template<class T> void print(vector<T> &vec){
-    for(int i=0;i<siz(vec);i++) cout<<vec[i]<<" ";cout<<endl;
-}
-template<class T> void print(set<T> &s) {
-    for(auto it: s) cout<<it<<" "; cout<<endl;
-}
-template<class T> void print(list<T> &lst) {
-    for(auto it: lst) cout<<it<<" "; cout<<endl;
-}
-template<class T> ll power(T a,int b){
-    ll po=1; while(b--) po*=a; return po;
+template<class T> ostream &operator<<(ostream &os, const pair<T, T> &a) { return os << a.ff << " " << a.ss; }
+ll power(ll a, int b) {
+    ll po = 1;
+    while (b) {
+        if (b & 1)
+            po *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return po;
 }
 
-template<class T> pair<T,T> operator+(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff+b.ff,a.ss+b.ss}; }
-template<class T> pair<T,T> operator-(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff-b.ff,a.ss-b.ss}; }
-template<class T> pair<T,T> operator*(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff*b.ff,a.ss*b.ss}; }
-template<class T> pair<T,T> operator%(const pair<T,T> &a,const pair<T,T> &b){ return {a.ff%b.ff,a.ss%b.ss}; }
-template<class T,class U> pair<T,T> operator+(const pair<T,T> &a,const U &b){ return { a.ff+b,a.ss+b}; }
-template<class T,class U> pair<T,T> operator*(const pair<T,T> &a,const U &b){ return { a.ff*b,a.ss*b}; }
+template<class T> pair<T, T> operator+(const pair<T, T> &a, const pair<T, T> &b) { return {a.ff + b.ff, a.ss + b.ss}; }
+template<class T> pair<T, T> operator-(const pair<T, T> &a, const pair<T, T> &b) { return {a.ff - b.ff, a.ss - b.ss}; }
+template<class T> pair<T, T> operator*(const pair<T, T> &a, const pair<T, T> &b) { return {a.ff * b.ff, a.ss * b.ss}; }
+template<class T> pair<T, T> operator%(const pair<T, T> &a, const pair<T, T> &b) { return {a.ff % b.ff, a.ss % b.ss}; }
+template<class T, class U> pair<T, T> operator+(const pair<T, T> &a, const U &b) { return {a.ff + b, a.ss + b}; }
+template<class T, class U> pair<T, T> operator*(const pair<T, T> &a, const U &b) { return {a.ff * b, a.ss * b}; }
 
-int Set(int N,int pos) { return N=N|(1<<pos); }
-int reset(int N,int pos){ return N=N&~(1<<pos);}
-bool check(int N,int pos){  return (bool) (N&(1<<pos));}
+int Set(int N, int pos) { return N = N | (1 << pos); }
+int reset(int N, int pos){ return N = N & ~ (1 << pos); }
+bool check(int N, int pos){ return (bool) (N & (1 << pos)); }
 
 ///=======================================template=======================================///
 
-#define LSOne(n) (n&(-n))
+//source: https://cp-algorithms.com/data_structures/fenwick.html
 
-class FT{
-    vi ft1,ft2;
-    int n;
-public:
-    FT(vi _ara){
-        n=siz(_ara);
-        ft1.assign(n+1,0);
-        ft2.assign(n+1,0);
-        fof(i,0,n){
-            update(i+1,i+1,_ara[i]);
-        }
+#define LSOne(n) (n & (-n))
+vi ft[2], ara; //ft[0] for b1 and ft[1] for b2
+int n;
+
+void add1(int idx, int val, int id) {
+    for (int i = idx ; i <= n; i += LSOne(i)) {
+        ft[id][i] += val;
+    }
+}
+
+void add(int l, int r, int val) {
+    add1(l, val, 0);
+    add1(r + 1, -val, 0);
+    add1(l, val * (l - 1), 1);
+    add1(r + 1, -val * r, 1);
+}
+
+ll sum(int i, int id) {
+    ll su = 0;
+    for (int x = i; x > 0; x -= LSOne(x))
+        su += ft[id][x];
+    return su;
+}
+
+ll pref_sum(int idx) {
+    return (sum(idx, 0) * idx - sum(idx, 1));
+}
+
+ll range_sum(int l, int r) {
+    return pref_sum(r) - pref_sum(l - 1);
+}
+int main() {
+
+    cin >> n;
+    ara.resize(n + 2);
+    ft[0].resize(n + 2);
+    ft[1] = ft[0];
+
+    for (int i = 0; i < n; i++) {
+        cin >> ara[i];
+        add(i + 1, i + 1, ara[i]);
     }
 
-    void add(vi &ft,int idx,int val){
-        for(;idx<=n;idx+=LSOne(idx)){
-            ft[idx]+=val;
-        }
-    }
+    int query;
+    cin >> query;
 
-    void update(int l,int r,int val){
-        add(ft1, l,val);
-        add(ft1,r+1,-val);
-        add(ft2,l,val*(l-1));
-        add(ft2,r+1,-val*r);
-    }
-
-    int sum(vi &ft,int idx){
-        int su=0;
-        while(idx){
-            su+=ft[idx];
-            idx-=LSOne(idx);
-        }
-        return su;
-    }
-
-    int rsq(int l,int r){
-        int su=0;
-        su=sum(ft1,r)*r-sum(ft2,r);
-        su-=(sum(ft1,l-1)*(l-1)-sum(ft2,l-1));
-        return su;
-    }
-
-};
-
-int main()
-{
-    //FI;FO;
-    FastIO;
-
-    vi vec;
-
-    int n; cin>>n;
-    fof(i,0,n){
-        vec.emplace_back(); cin>>vec.back();
-    }
-
-    FT ft(vec);
-
-    int q;
-    cin>>q;
-    while(q--){
-        int u,v,add;
-        cin>>u>>v>>add;
-        ft.update(u,v,add);
-        cout<<ft.rsq(u+1,v)<<endl;
+    while (query--) {
+        int x, y;
+        cin >> x >> y;
+        cout << range_sum(x, y) << endl;
+        int l, r, val;
+        cin >> l >> r >> val;
+        add(l, r, val);
     }
 
     return 0;
